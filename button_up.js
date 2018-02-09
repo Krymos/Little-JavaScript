4f92a70a372ba9ea5fcde6fa6ea5237e05739c68
Katya <krymos@mail.ru>
2018-02-09 19:55:35 +0300

    Create button_up.js

---
+++ button_up.js
@@ -0,0 +1,24 @@
+let upArrow = document.createElement('div');
+upArrow.id = 'up-arrow';
+
+document.body.appendChild(upArrow);
+
+window.onscroll = showAndHideUpArrow;
+
+let getUpArrow = document.getElementById('up-arrow');
+
+getUpArrow.onclick = scrollTop;
+
+function showAndHideUpArrow() {
+
+	if (window.scrollY > 450) {
+		upArrow.style.display = "block";
+	} else {
+		upArrow.style.display = "none";
+	}
+
+}
+
+function scrollTop() { 
+	window.scrollTo(0, 0);
+}
